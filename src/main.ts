7f874c65c401e1309d9a5133ddb7eec8ab91e546
huynguyen <huy19th@gmail.com>
2024-04-03 00:54:56 +0700

    interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { TimeExeInterceptor } from './interceptors/time-execution.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.enableCors();
   app.useBodyParser('json', { limit: '10mb' });
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TimeExeInterceptor());
   await app.listen(port);
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,12 +2,15 @@ import {
     Controller,
     Body,
     Post,
+    UseInterceptors
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LogInResponse } from './auth.interface';
 import { LogInDto, RegisterDto } from './auth.dto';
 import { Public } from './public.decorator';
+import { TimeExeInterceptor } from 'src/interceptors/time-execution.interceptor';
 
+// @UseInterceptors(TimeExeInterceptor) // bind interceptor to controller
 @Public()
 @Controller('auth')
 export class AuthController {
