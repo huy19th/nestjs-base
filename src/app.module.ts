9673efe2f76647eeaec5170087a880433fbf69c9
huynguyen <huy19th@gmail.com>
2024-04-02 23:56:21 +0700

    user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { configuration } from './config/configuration';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './config/mongo.config';
 import { CacheModule } from './providers/cache/cache.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
     MongooseModule.forRootAsync({ useClass: MongooseConfigService, imports: [ConfigModule] }),
     CacheModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Param, Body, Get, Post, Patch, Delete } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './users.dto';
+import { UsersService } from './users.service';
+import { UserDocument } from './users.schema';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private usersService: UsersService) { }
+
+    @Get(':id')
+    async getProfile(@Param('id') id: string) {
+        const profile = await this.usersService.getUserProfile(id);
+        return profile;
+    }
+
+    @Post()
+    async createUser(@Body() body: CreateUserDto): Promise<UserDocument> {
+        const user: UserDocument = await this.usersService.create(body);
+        return user;
+    }
+
+    @Patch(':id')
+    async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto): Promise<UserDocument> {
+        const user: UserDocument = await this.usersService.updateUser(id, body);
+        return user;
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<string> {
+        await this.usersService.deleteUser(id);
+        return 'success';
+    }
+}
