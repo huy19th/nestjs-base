0afca6912397950cea23e203809ae8f21eefae46
huynguyen <huy19th@gmail.com>
2024-04-02 23:06:37 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+require('dotenv').config();
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useBodyParser('json', { limit: '10mb' });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 8080);
+}
+bootstrap();
