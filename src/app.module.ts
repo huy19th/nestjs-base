44d0beb114bc69aee56de51c49b733dc86392c2c
huynguyen <huy19th@gmail.com>
2024-04-02 23:54:24 +0700

    redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config/configuration';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './config/mongo.config';
+import { CacheModule } from './providers/cache/cache.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
     MongooseModule.forRootAsync({ useClass: MongooseConfigService, imports: [ConfigModule] }),
+    CacheModule,
   ],
   controllers: [],
   providers: [],
