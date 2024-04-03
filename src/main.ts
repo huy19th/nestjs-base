104b1466e605d158878195fdacd08e55996e1aef
huynguyen <huynguyen@myg.vn>
2024-04-03 17:19:51 +0700

    fastify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true })
+  );
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   app.enableCors();
-  app.useBodyParser('json', { limit: '10mb' });
+  app.useBodyParser('application/json', { bodyLimit: 10 * 1000 * 1024 });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
