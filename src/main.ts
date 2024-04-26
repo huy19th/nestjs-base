06b4d035209adb0e5c2c82b0c09e62e97dac2e1e
huynguyen <huy19th@gmail.com>
2024-04-26 23:13:26 +0700

    multer memory storage

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.enableCors();
   app.useBodyParser('json', { limit: '10mb' });
   app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, './public'));
   await app.listen(port);
 }
 bootstrap();
