9038b073f52d4d49698180773c9c652ef283072a
huynguyen <huy19th@gmail.com>
2024-05-04 23:37:30 +0700

    redis adapter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { RedisIoAdapter } from './config/redis-io-adapter.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,11 @@ async function bootstrap() {
   app.enableCors();
   app.useBodyParser('json', { limit: '10mb' });
   app.useGlobalPipes(new ValidationPipe());
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
+  
   await app.listen(port);
 }
 bootstrap();
